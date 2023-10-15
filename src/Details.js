4a0f3b8362ca484c9593163f4434dd185ffc2e93
zergiorvdr <imamasyari330@gmail.com>
2023-10-16 00:04:46 +0700

    firstpush

---
+++ src/Details.js
@@ -0,0 +1,152 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project.jpeg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Imam A'syari",
+  tagline: "I build things for web",
+  img: profile,
+  about: `As a web developer, I am a creative and technical individual with an innovative ability to deliver high-quality solutions. I tend to be collaborative, detail-oriented, and operate with multidisciplinary skills. My analytical and problem-solving abilities enable me to work independently, produce user-focused interfaces, and ensure high-quality project outcomes. I am a learning enthusiast, always open to new technologies, and adept at adapting to changes, making me a proactive, organized, and efficient web developer.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://github.com/zergiorvdr",
+  twitter: "https://twitter.com/",
+  instagram: "https://instagram.com/lovinsmwnn?igshid=OGQ5ZDc2ODk2ZA==",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Internship",
+    Company: `TrustedTech Global Solutions`,
+    Location: "Indonesia",
+    Type: "Internship",
+    Duration: "Sep 2023 - Oct 2023",
+  }
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Frontend Development",
+    Company: "Udemy, YouTube, Google, Medium",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Present",
+  }
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Telegram Web Apps",
+    image: projectImage1,
+    description: `Telegram web apps is a version of the Telegram messaging application that can be accessed through a web browser without the need to download or install a specific app. This allows users to send messages, share files, create groups, and utilize other features directly from their computer or mobile device with an internet connection. The web apps Telegram interface often closely resembles the original Telegram app, making it easy for users to seamlessly switch between the two platforms. Security and data encryption remain top priorities in the use of Telegram web apps.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "http://turbo-memory.vercel.app/",
+    githubLink: "https://github.com/zergiorvdr/turbo-memory",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "imamasyari630@gmail.com",
+  phone: "+6285220601095",
+};
